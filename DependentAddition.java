299f3fbe76c3a495d2591f52747da1a1b8842514
Nuseiba Al-Harazi <101468552+alharazinuseiba1@users.noreply.github.com>
2023-10-04 14:43:46 -0400

    Add files via upload
    Class Exercise to create 3 dependent addition methods

---
+++ DependentAddition.java
@@ -0,0 +1,44 @@
+package PA;
+public class DependentAddition{
+
+    private DependentAddition dependent;
+    private String name;
+    private String dateOfBirth;
+    private String address;
+    private int alienNum;
+    private String applicantName; 
+    private int applicantAlienNum;
+    private String applicantEmail;
+
+    public static void main(String[] args){
+    }
+
+    private DependentAddition(String name, String dateOfBirth, String address, int alienNum, 
+                                String applicantName, int applicantAlienNum, String applicantEmail)
+    {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.alienNum = alienNum;
+        this.applicantName = applicantName;
+        this.applicantAlienNum = applicantAlienNum;
+        this.applicantEmail = applicantEmail;
+    }
+
+    public void dependentCreation(String name, String dateOfBirth, String address, int alienNum, 
+                                    String applicantName, int applicantAlienNum, String applicantEmailString )
+    {
+                  this.dependent = new DependentAddition(name, dateOfBirth, address, alienNum, 
+                                                        applicantName,applicantAlienNum,applicantEmail);                      
+    }
+
+    public DependentAddition getDependentAddition(){
+
+        return this.dependent;
+
+    }
+
+    public String validateDependent(DependentAddition da){
+        return "ok";
+    }
+}
